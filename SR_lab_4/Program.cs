4f8d18a97a3f95e69629141fc544ce0b76a2e135
RaDioDa <93476589+RaDioDa@users.noreply.github.com>
2021-11-18 22:27:17 +0300

    Add files via upload

---
+++ SR_lab_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SR_lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fail1 = 2;
+            int fail2 = 86;
+            int work = 3*3600;
+
+            int m = (fail1 + fail2)/2;
+
+            double tt = 0;
+            int t = 0;
+
+            for(int i = 0; t < work; i++)
+            {
+                t = (i*(i+1)/2) * m;
+                tt += t;
+            }
+            Console.WriteLine(tt/3600);
+        }
+    }
+}
